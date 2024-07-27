4d96f6e2f8db6410ab79a7603d7fe3a2c2ab06ef
Paulo Vitor Ricken <campestrini.paulo1@gmail.com>
2024-07-26 21:32:30 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    
+    <?php
+      
+      Echo 'Olá Mundo'
+
+    ?>
+    <hr>
+     <div>
+       <a href="index.html">volte para a pagina principal</a>
+    </div>
+</body>
+</html>
